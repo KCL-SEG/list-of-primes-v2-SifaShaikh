b6c2f9efd10541e251339ba82d711cabc2dd771f
SifaShaikh <114479739+SifaShaikh@users.noreply.github.com>
2022-12-09 09:22:18 +0000

    Added a while loop and foor loop - along side an if statement to generate a list of prime numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if (number_of_primes<1):
+        print("0 or negative values are not accepted")
+
     list = []
+    count = 0
+    while count<number_of_primes:
+        for n in range(2,prime_nums):
+            if (prime_nums % n == 0):
+                break
+            else:
+                count+=1
+                list.append(prime_nums)
+            prime_nums+=1
+
     return list
